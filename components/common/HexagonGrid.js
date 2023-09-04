5c8db9b3800579298a8e8c5697a3ef5790e4e429
Sirisha Ongole <ongole.sirisha@gmail.com>
2023-09-04 08:53:41 -0400

    HexGrid viewbox calc to center in col

--- components/common/HexagonGrid.js
+++ components/common/HexagonGrid.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { HexGrid, Layout, Hexagon, Text } from "react-hexgrid";
 
 export const HexagonGrid = ({ gridData, setShowView, updateSelected }) => {
+  const [viewBox, setViewBox] = useState("0 0 0 0");
+
+  //Calculate viewBox for HexGrid
+  useEffect(() => {
+    const hexGrid = document.querySelector(".hexGridHolder");
+    const hexGridWidth = hexGrid.clientWidth;
+    const hexGridHeight = hexGrid.clientHeight;
+    const hexGridRatio = hexGridWidth / hexGridHeight;
+    const viewBoxWidth = 300;
+    const viewBoxHeight = 130;
+    const viewBoxRatio = viewBoxWidth / viewBoxHeight;
+
+    const offset = -0.04 * viewBoxWidth * hexGridRatio;
+
+    console.log(hexGridWidth, hexGridHeight, hexGridRatio, viewBoxRatio);
+
+    if (hexGridRatio > viewBoxRatio) {
+      setViewBox(`0 0 ${viewBoxWidth} ${viewBoxWidth / hexGridRatio}`);
+    } else {
+      setViewBox(
+        `-${(viewBoxHeight * hexGridRatio) / 2 + offset} -${
+          viewBoxHeight / 2
+        } ${viewBoxHeight * hexGridRatio} ${viewBoxHeight}`
+      );
+    }
+  }, []);
+
   return (
     <div
       className="hexGridHolder"
@@ -19,8 +46,8 @@ export const HexagonGrid = ({ gridData, setShowView, updateSelected }) => {
       >
         {gridData.title}
       </h5>
-      <HexGrid width={250} height={250} viewBox="-30 -50 100 100">
-        <Layout size={{ x: 10, y: 10 }} spacing={1.1}>
+      <HexGrid width={250} height={330} viewBox={viewBox}>
+        <Layout size={{ x: 15, y: 15 }} spacing={1.1}>
           <Hexagon q={0} r={0} s={0}>
             <Text>{gridData.title}</Text>
           </Hexagon>
